4f9e755810848db06915e6347f786e518a8e014c
Khamphouvong <Khamphouvong.kcsv@gmail.com>
2018-08-07 13:49:48 +0700

    update

--- app/src/main/java/tia/androidlaos/la/touralaos/fragment/MainFragment.java
+++ app/src/main/java/tia/androidlaos/la/touralaos/fragment/MainFragment.java
@@ -7,10 +7,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import tia.androidlaos.la.touralaos.R;
 
 public class MainFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+    }    // Method Main
 
     @Nullable
     @Override
