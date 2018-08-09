a7bed636f3ef66c2cd0d1d8871520713e2e2e111
Khamphouvong <Khamphouvong.kcsv@gmail.com>
2018-08-09 17:22:52 +0700

    End 9AUG

--- app/src/main/java/tia/androidlaos/la/touralaos/fragment/MainFragment.java
+++ app/src/main/java/tia/androidlaos/la/touralaos/fragment/MainFragment.java
@@ -1,5 +1,7 @@
 package tia.androidlaos.la.touralaos.fragment;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,13 +9,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import tia.androidlaos.la.touralaos.R;
+import tia.androidlaos.la.touralaos.ServiceActivity;
+import tia.androidlaos.la.touralaos.utility.MyAlert;
+import tia.androidlaos.la.touralaos.utility.UserModel;
 
 public class MainFragment extends Fragment {
+
+    private String emailString, passwordString;
+    private ProgressDialog progressDialog;
+
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -25,15 +45,80 @@ public class MainFragment extends Fragment {
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
+
+
     }    // Method Main
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                //Get Value
+                emailString = emailEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString();
+
+                //Check Blank
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.normalDialog("Have Space",
+                            "Please Fill Every Blank");
+                } else {
+                    checkAuthen();
+                }
+
+            }
+        });
+    }
+
+
+    private void checkAuthen() {
+
+        //createProgressDialog
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Login Process");
+        progressDialog.setMessage("Please Wait....");
+        progressDialog.show();
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity(), "Welcome", Toast.LENGTH_SHORT).show();
+                            moveToService();
+                            progressDialog.dismiss();
+                        } else {
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.normalDialog("Login Failed",
+                                    "Because ==>" + task.getException().getMessage());
+                        }
+
+                    }
+                });
+
+    }
 
     private void checkStatus() {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         if (firebaseAuth.getCurrentUser() != null) {
-            getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.contentMainFragment, new ServiceFragment())
-                    .commit();
+            moveToService();
         }
+    }
+
+    private void moveToService() {
+
+        startActivity(new Intent(getActivity(), ServiceActivity.class));
+        getActivity().finish();
+
+
     }
 
     private void registerController() {
